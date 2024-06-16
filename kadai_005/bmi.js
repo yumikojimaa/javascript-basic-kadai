a51124d0af8a2ad3d6d00d9ac95b803c3eb3fb90
yumi <y.charmmykitty.15milk@gmail.com>
2024-06-16 18:03:27 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,9 @@
+// 体重と身長の変数に値を代入する
+let weightKg = 68;   // 体重（kg）
+let heightM = 1.7;   // 身長（m）
+
+// BMIの計算式を実行する
+let bmi = weightKg / (heightM * heightM);
+
+// BMIの値をコンソールに出力する
+console.log("BMI:", bmi);
